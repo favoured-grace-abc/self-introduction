6bc5b216673cbc378d3ef8767fe2fa7dc335714b
Nathan Grace <favouramaraci1@gmail.com>
2026-03-16 02:24:10 +0100

    feat: string-inspector

---
+++ string-inspector.js
@@ -0,0 +1,8 @@
+
+
+let word = "companion"
+
+console.log(`length: ${word.length}`);
+console.log(`first character: ${word[0]}`);
+console.log(`last character: ${word[word.length - 1]}`);
+console.log(`reversed: ${word.split("").reverse().join("")}`);
