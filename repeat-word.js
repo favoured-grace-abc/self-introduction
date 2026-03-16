3903d819ffeeb16f935332f88b0e0a35f62326f6
Nathan Grace <favouramaraci1@gmail.com>
2026-03-16 02:39:57 +0100

    feat:repeat-word

---
+++ repeat-word.js
@@ -0,0 +1,12 @@
+let word = "Hello"
+let times = 5;
+for (let i = 0; i < times; i++) {
+  console.log(word)
+}
+
+console.log("\nSecond run:");
+word = "Javascript";
+times = 3;
+for (let i = 0; i < times; i++) {
+  console.log(word)
+}
