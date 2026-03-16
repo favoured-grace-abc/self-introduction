b59e9bf7eda5d9250ed7a303da63ceccc4f8da54
Nathan Grace <favouramaraci1@gmail.com>
2026-03-16 02:56:10 +0100

    feat:string-checker

---
+++ string-checker.js
@@ -0,0 +1,7 @@
+
+
+let sentence =  "Javascript is fun";
+console.log(`length: ${sentence.length}`);
+console.log(`uppercase: ${sentence.toUpperCase()}`);
+console.log(`lowercase: ${sentence.toLowerCase()}`);
+console.log(`includes "Javascript": ${sentence.includes("Javascript")}`);
