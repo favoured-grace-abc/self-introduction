36b617303aebb8912de9ed2ee322cd7ac7b5f358
Nathan Grace <favouramaraci1@gmail.com>
2026-03-16 02:14:57 +0100

    feat: self-introduction

---
+++ self-introduction.js
@@ -0,0 +1,11 @@
+// Exercise 1: Self-Introduction 
+
+let firtName = "Nathan"
+let lastName = "Amarachi"
+let age = 26;
+let country = "Nigeria"
+let hobby = "Reading";
+
+console.log(`My full name is ${firtName} ${lastName}.`);
+console.log(`I am ${age} years young and I live in ${country}.`);
+console.log(`In my free time, I enjoy ${hobby}.`);
